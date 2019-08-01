c3ec85d9c867518d831beba66b1aba2ed62a07d5
lalalazero <zzzero520@hotmail.com>
2019-08-01 11:24:48 +0800

    npm publish 初体验

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,16 +1,3 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
 import Icon from './icon/Icon'
 
-const fn: React.MouseEventHandler<SVGElement> = (e) => {
-    console.log(e.target)
-}
-ReactDOM.render(
-    <div>
-        <Icon name="wechat"
-            onClick={fn}
-            onMouseEnter={() => { console.log('enter') }}
-            onMouseLeave={() => { console.log('leave') }}
-        ></Icon>
-        <Icon name="alipay"></Icon>
-    </div>, document.querySelector('#root'))
+export { Icon }
