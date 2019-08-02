f1af498cb6dd11247dce41bad254af5bc899b16b
lalalazero <zzzero520@hotmail.com>
2019-08-02 15:43:43 +0800

    完成 deploy.sh 脚本配置，在 master 分支上运行 deploy.sh 脚本会一并推送到 deploy 分支进行发布

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/env bash
 if [ $1 ]; then
     # echo $1
-    npm version $1 && \
+    npm version $1 && git push && \
     git checkout deploy && \
     git merge master && \
     git push
 else
-    npm version patch && \
+    npm version patch && git push && \
     git checkout deploy && \
     git merge master && \
     git push
