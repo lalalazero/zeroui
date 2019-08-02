798a67a4bccd96614c92cb37d41e6492978f6d3d
lalalazero <zzzero520@hotmail.com>
2019-08-02 17:17:05 +0800

    创建初步的示例首页 example.tsx

--- example.tsx
+++ example.tsx
@@ -1,6 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Icon from './lib/icon/Icon'
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
+import IconExample from './lib/icon/Icon.example'
+import ButtonExample from './lib/button/Button.example'
 
 
-ReactDOM.render(<Icon name="alipay"></Icon>, document.querySelector('#root'))
+ReactDOM.render(
+    <Router>
+        <div>
+            <header>
+                <div className="logo">
+                    ZeroUI
+        </div>
+            </header>
+            <div>
+                <aside>
+                    <h2>组件</h2>
+                    <ul>
+                        <li>
+                            <Link to="/icon">Icon</Link>
+                        </li>
+                        <li>
+                            <Link to="/button">Button</Link>
+                        </li>
+                    </ul>
+                </aside>
+                <main>
+                    <Route path="/icon" component={IconExample}></Route>
+                    <Route path="/button" component={ButtonExample}></Route>
+                </main>
+            </div>
+        </div>
+    </Router>, document.querySelector('#root'))
---
+++ lib/icon/Icon.example.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Icon from './Icon'
+const IconExample:React.FunctionComponent = () =>{
+    return (
+        <div>
+            <Icon name="alipay"></Icon>
+        </div>
+    )
+}
+
+export default IconExample
