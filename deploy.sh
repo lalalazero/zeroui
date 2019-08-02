759a0cdeb42a90b9e5cc8e8187c1f667decd32d2
lalalazero <zzzero520@hotmail.com>
2019-08-02 15:32:03 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,11 @@
 #!/bin/env bash
 if [ $1 ]; then
-    echo $1
+    # echo $1
     git checkout deploy && \
     git merge master && \
     npm version $1 && git push
+else
+    git checkout deploy && \
+    git merge master && \
+    npm version patch && git push
 fi
