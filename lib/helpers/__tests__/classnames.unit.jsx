d8be2b9e15c61bcb6866ad583eebd522e4605102
lalalazero <zzzero520@hotmail.com>
2020-04-07 16:40:35 +0800

    test:  添加 scopedClassMaker 测试用例

--- lib/helpers/__tests__/classnames.unit.jsx
+++ lib/helpers/__tests__/classnames.unit.jsx
@@ -1,4 +1,6 @@
 import classnames from '../classes'
+import { scopedClassMaker } from '../classes'
+
 describe('classnames 测试', () => {
     it('接受1个 classname', () => {
         const result = classnames('a')
@@ -12,5 +14,21 @@ describe('classnames 测试', () => {
     })
     it('什么都不传', () => {
         expect(classnames()).toEqual('')
+    })
+})
+
+describe('scopedClassMaker 测试', () => {
+    it('接受字符串或者对象', (done) => {
+        const sc = scopedClassMaker('zeroUI-layout')
+        expect(sc('')).toEqual('zeroUI-layout')
+        expect(sc('header')).toEqual('zeroUI-layout-header')
+        expect(sc({ hasSider: true })).toEqual('zeroUI-layout zeroUI-layout-has-sider')
+        expect(sc({ hasSider: true, active: true })).toEqual('zeroUI-layout zeroUI-layout-has-sider zeroUI-layout-active')
+
+        expect(sc('', 'userclass')).toEqual('zeroUI-layout userclass')
+        expect(sc('header', 'userclass')).toEqual('zeroUI-layout-header userclass')
+        expect(sc({ hasSider: true }, 'userclass')).toEqual('zeroUI-layout zeroUI-layout-has-sider userclass')
+        done()
+
     })
 })
