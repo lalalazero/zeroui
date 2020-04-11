c90bbcca6db36552d6bbcf91b258eb60a2610c0f
lalalazero <zzzero520@hotmail.com>
2020-04-11 22:34:59 +0800

    feat: refactor icon example and add clip-to-board

--- example/icon/Icon.example.tsx
+++ example/icon/Icon.example.tsx
@@ -1,74 +1,38 @@
 import React from 'react'
+import copy from 'copy-to-clipboard';
 import { Icon } from '../../components'
 import './Icon.example.scss'
 const IconExample:React.FunctionComponent = () =>{
+    const ICONS = [
+        'like',
+        'info',
+        'error',
+        'loading',
+        'alipay',
+        'wechat',
+        'left',
+        'right',
+        'down',
+        'setting',
+        'emptysearch',
+        'filled-up',
+        'filled-down',
+        'download',
+        'next-double',
+        'prev-double'
+    ]
+    const onClick = (name:string):void => {
+        const text = `<Icon name="${name}"></Icon>`
+        copy(text)
+        window.confirm(text + ' copied!')
+    }
     return (
         <div className='icon-example'>
+            <p>内置的图标，点击可复制使用</p>
             <ul>
-                <li>
-                    <Icon name="like"></Icon><br/>
-                    <span>like</span>
-                </li>
-                <li>
-                    <Icon name="info"></Icon><br/>
-                    <span>info</span>
-                </li>
-                <li>
-                    <Icon name="error"></Icon><br/>
-                    <span>error</span>
-                </li>
-                <li>
-                    <Icon name="loading"></Icon><br/>
-                    <span>loading</span>
-                </li>
-                <li>
-                    <Icon name="alipay"></Icon><br/>
-                    <span>alipay</span>
-                </li>
-                <li>
-                    <Icon name="wechat"></Icon><br/>
-                    <span>wechat</span>
-                </li>
-                <li>
-                    <Icon name="left"></Icon><br/>
-                    <span>left</span>
-                </li>
-                <li>
-                    <Icon name="right"></Icon><br/>
-                    <span>right</span>
-                </li>
-                <li>
-                    <Icon name="down"></Icon><br/>
-                    <span>down</span>
-                </li>
-                <li>
-                    <Icon name="setting"></Icon><br/>
-                    <span>setting</span>
-                </li>
-                <li>
-                    <Icon name="emptysearch"></Icon><br/>
-                    <span>emptysearch</span>
-                </li>
-                <li>
-                    <Icon name="filled-up"></Icon><br/>
-                    <span>filled-up</span>
-                </li>
-                <li>
-                    <Icon name="filled-down"></Icon><br/>
-                    <span>filled-down</span>
-                </li>
-                <li>
-                    <Icon name="download"></Icon><br/>
-                    <span>download</span>
-                </li>
-                <li>
-                    <Icon name="next-double"></Icon><br/>
-                    <span>next-double</span>
-                </li>
-                <li>
-                    <Icon name="prev-double"></Icon><br/>
-                    <span>prev-double</span>
-                </li>
+                {
+                    ICONS.map(name => (<li key={name} onClick={()=>onClick(name)}><Icon name={name}></Icon><br/><span>{name}</span></li>))
+                }
             </ul>
         </div>
     )
