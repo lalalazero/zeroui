b4039b9bd9f2349635cadad254b7060932caa577
lalalazero <zzzero520@hotmail.com>
2020-07-13 20:21:36 +0800

    add mini-css-extract-plugin, clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 module.exports = {
     entry: {
         index: './components/index.tsx'
@@ -32,7 +34,9 @@ module.exports = {
             },
             {
                 test: /\.s([ac])ss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                use: [
+                    MiniCssExtractPlugin.loader
+                    , 'css-loader', 'sass-loader']
             },
             {
                 test: /\.md$/,
@@ -42,5 +46,11 @@ module.exports = {
                 //   ]
             }
         ]
-    }
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+        new CleanWebpackPlugin()
+    ]
 }
