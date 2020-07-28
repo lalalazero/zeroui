546db715ff35fd0b485a91add9c56826da407ac2
lalalazero <zzzero520@hotmail.com>
2020-07-28 16:24:05 +0800

    fix: publicPath incorrect

--- webpack.config.doc.js
+++ webpack.config.doc.js
@@ -11,7 +11,7 @@ module.exports = merge(base, {
     },
     output: {
         path: path.resolve(__dirname, 'dist/'),
-        publicPath: '/lalalazero'
+        publicPath: '/ZEROUI'
     },
     plugins: [
         new HtmlWebpackPlugin({
