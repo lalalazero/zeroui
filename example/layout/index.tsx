4b654da5d6af833ac4254033f7ee289a66e8e7d9
lalalazero <zzzero520@hotmail.com>
2020-10-15 21:58:21 +0800

    改写 layout 示例

---
+++ example/layout/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import DemoRenderer from '../DemoRenderer'
+import './style.scss'
+
+export default class Demo extends React.Component<any, any> {
+    constructor(props: any) {
+        super(props)
+        this.state = {
+            demos: [],
+            api: '',
+        }
+    }
+    async componentDidMount() {
+        const demos = [await import('./01-基本布局.mdx')]
+        // const api = await import('./api.mdx')
+        this.setState({
+            // api: api.default.apiContent,
+            demos,
+        })
+    }
+
+    render() {
+        const { demos, api } = this.state
+        const className = 'zeroUI-layout-example'
+        return (
+            <DemoRenderer
+                className={className}
+                layout={24}
+                demos={demos}
+                api={api}
+            ></DemoRenderer>
+        )
+    }
+}
