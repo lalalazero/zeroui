463120b166df00ded2b1bd411c4cd1ed74a8d557
lalalazero <zzzero520@hotmail.com>
2020-10-15 14:44:23 +0000

    Deploying to gh-pages from  @ e2e0a46e771b3f84ab0ba6c3af479844eb21e2c2 🚀

--- example/menu/Menu.example.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference types="react" />
-export default function MenuDemo(): JSX.Element;
--- example/tooltip/Tooltip.example.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="react" />
-import './Tooltip.example.scss';
-export default function ToolTipDemo(): JSX.Element;
