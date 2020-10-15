a7c0000bc9a5bedc661f7dda1c0ff7acae2c2079
lalalazero <zzzero520@hotmail.com>
2020-10-15 12:50:49 +0000

    Deploying to gh-pages from  @ 19f49ae6509fd1fa2ef15e0015173c7c334d536e 🚀

--- example/button/Button.example.d.ts
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-export default class ButtonDemo extends React.Component<any, any> {
-    constructor(props: any);
-    componentDidMount(): void;
-    render(): JSX.Element;
-}
--- example/button/index.d.ts
+++ example/button/index.d.ts
@@ -1,3 +1,8 @@
-import { FunctionComponent } from 'react';
-declare const ButtonDemo: FunctionComponent;
-export default ButtonDemo;
+import React from 'react';
+import './Button.example.scss';
+export default class ButtonDemo extends React.Component<any, any> {
+    constructor(props: any);
+    componentDidMount(): Promise<void>;
+    renderDemo(files: any[]): void;
+    render(): JSX.Element;
+}
