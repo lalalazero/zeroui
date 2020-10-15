997a7d1e0c1312407e1b1a786fe7361a06130cbf
lalalazero <zzzero520@hotmail.com>
2020-10-15 21:53:27 +0800

    改写 grid 的示例

--- example/grid/index.tsx
+++ example/grid/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import DemoRenderer from '../DemoRenderer'
 import './style.scss'
 
-export default class ButtonDemo extends React.Component<any, any> {
+export default class Demo extends React.Component<any, any> {
     constructor(props: any) {
         super(props)
         this.state = {
@@ -12,14 +12,14 @@ export default class ButtonDemo extends React.Component<any, any> {
     }
     async componentDidMount() {
         const demos = [
-            await import('./01-基础类型.mdx'),
-            await import('./02-图标按钮.mdx'),
-            await import('./03-按钮大小.mdx'),
-            await import('./04-加载中状态.mdx'),
-            await import('./05-不可用状态.mdx'),
-            await import('./06-按钮组.mdx'),
-            await import('./07-幽灵按钮.mdx'),
-            await import('./08-块级按钮.mdx'),
+            await import('./01-基础栅格.mdx'),
+            await import('./02-gutter间隔.mdx'),
+            await import('./03-左右偏移.mdx'),
+            await import('./04-栅格排序.mdx'),
+            await import('./05-flex布局.mdx'),
+            await import('./06-flex对齐.mdx'),
+            await import('./07-flex排序.mdx'),
+            await import('./08-响应式布局.mdx'),
         ]
         const api = await import('./api.mdx')
         this.setState({
@@ -30,7 +30,7 @@ export default class ButtonDemo extends React.Component<any, any> {
 
     render() {
         const { demos, api } = this.state
-        const className = 'zeroUI-button-example'
+        const className = 'zeroUI-grid-example'
         return (
             <DemoRenderer
                 className={className}
