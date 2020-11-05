19c974df2b949d6eb4fb32507003860674c7bc6b
lalalazero <zzzero520@hotmail.com>
2020-11-05 10:27:25 +0000

    Deploying to gh-pages from  @ 1e0a4b2cb4e5ed7de86ca3c2b05ba4f89267045b 🚀

--- components/notification/Notification.d.ts
+++ components/notification/Notification.d.ts
@@ -1,11 +1,16 @@
 import React from 'react';
 import './style.scss';
-declare type NotificationConfig = {
-    getContainer?: () => HTMLElement;
+declare const NotificationPlacementTypes: ["topRight", "topLeft", "topCenter", "bottomLeft", "bottomRight", "bottomCenter"];
+export declare type NotificationPlacement = typeof NotificationPlacementTypes[number];
+export declare type NotificationConfig = {
     title?: React.ReactNode;
     body?: React.ReactNode;
     wait?: number;
     autoClose?: boolean;
+};
+export declare type InstanceConfig = {
+    placement?: NotificationPlacement;
+    getContainer?: () => HTMLElement;
 };
 declare class Notification {
     container: Element;
@@ -16,7 +21,7 @@ declare class Notification {
     private constructor();
     remove(seed: number): void;
     open(config: NotificationConfig): void;
-    static getIntance(): Notification;
+    static getIntance(config?: InstanceConfig): Notification;
     static destroy(instance: Notification): undefined;
 }
 export default Notification;
