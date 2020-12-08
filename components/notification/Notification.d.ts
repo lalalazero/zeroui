d20671160cba588d42382057c38f364167e3dfe1
lalalazero <zzzero520@hotmail.com>
2020-12-08 10:54:37 +0000

    Deploying to gh-pages from  @ e8c0135df99f2df6108c3ecc75718aae13ed277d 🚀

--- components/notification/Notification.d.ts
+++ components/notification/Notification.d.ts
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import './style.scss';
 declare const NotificationPlacementTypes: ["topRight", "topLeft", "topCenter", "bottomLeft", "bottomRight", "bottomCenter"];
 export declare type NotificationPlacement = typeof NotificationPlacementTypes[number];
@@ -7,21 +7,38 @@ export declare type NotificationConfig = {
     body?: React.ReactNode;
     wait?: number;
     autoClose?: boolean;
+    placement?: NotificationPlacement;
 };
+interface NotificationItemProps {
+    seed: string;
+    title: string;
+    body: ReactNode;
+    onClose: () => void;
+    wait: number;
+    autoClose: boolean;
+}
+declare const NotificationItem: React.FC<NotificationItemProps>;
 export declare type InstanceConfig = {
     placement?: NotificationPlacement;
     getContainer?: () => HTMLElement;
 };
+interface NotificationItem extends NotificationConfig {
+    seed: number;
+    onClose: any;
+}
 declare class Notification {
     container: Element;
     root: Element;
     mountNode: Element;
     seed: number;
-    notifications: any[];
+    notifications: NotificationItem[];
+    instance: Notification | null;
     private constructor();
+    private initRoot;
+    private adjustPlacement;
     remove(seed: number): void;
     open(config: NotificationConfig): void;
-    static getIntance(config?: InstanceConfig): Notification;
+    static getIntance(): Notification;
     static destroy(instance: Notification): undefined;
 }
 export default Notification;
