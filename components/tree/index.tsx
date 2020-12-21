9574e042e6c5eec0c455f8d539769930de3054f6
lalalazero <zzzero520@hotmail.com>
2020-12-21 18:42:35 +0800

    tree select 2

--- components/tree/index.tsx
+++ components/tree/index.tsx
@@ -1,28 +1,35 @@
-import React, { ChangeEvent, useEffect, useState } from 'react'
+import React, {
+    ChangeEvent,
+    ChangeEventHandler,
+    useEffect,
+    useMemo,
+    useRef,
+    useState,
+} from 'react'
 import { classname } from '../_util/classes'
 import './style.scss'
 
 const PREFIX = 'zeroui-tree'
 
-export type TreeItem = {
+export type TreeNode = {
     title: string
     key: string
-    children?: TreeItem[]
+    children?: TreeNode[]
 }
 
-export type TreeData = TreeItem[]
+export type TreeData = TreeNode[]
 
 export type TreeProps = {
-    treeData: TreeItem[]
+    treeData: TreeNode[]
     checkedKeys?: string[]
     onCheck?: (
         newCheckedKeys: string[],
-        checkItem?: TreeItem,
+        checkItem?: TreeNode,
         e?: ChangeEvent<HTMLInputElement>
     ) => void
 }
 
-const getChildrenKeys = (parentNode: TreeItem) => {
+const getChildrenKeys = (parentNode: TreeNode) => {
     const collectChildrenKey = (children: TreeData) => {
         let keys: string[] = []
         children.forEach((child) => {
@@ -37,6 +44,114 @@ const getChildrenKeys = (parentNode: TreeItem) => {
     return collectChildrenKey(parentNode.children || [])
 }
 
+interface TreeNodeProps {
+    treeNode: TreeNode
+    checkedKeys: string[]
+    setCheckedKeysFn: (checkedKeys: string[]) => void
+    onCheck?: (
+        newCheckedKeys: string[],
+        checkItem?: TreeNode,
+        e?: ChangeEvent<HTMLInputElement>
+    ) => void
+    level: number
+}
+
+const TreeNode: React.FC<TreeNodeProps> = (props) => {
+    const ref = useRef<HTMLInputElement>(null)
+    const childrenKeys = useMemo(() => {
+        if (props.treeNode) {
+            return getChildrenKeys(props.treeNode)
+        }
+        return []
+    }, [props.treeNode])
+
+    const isChildrenAllChecked =
+        childrenKeys.length > 0 &&
+        childrenKeys.every((key) => props.checkedKeys.indexOf(key) >= 0)
+
+    const isChildrenAllUnchecked =
+        childrenKeys.length > 0 &&
+        childrenKeys.every((key) => props.checkedKeys.indexOf(key) < 0)
+
+    const isChecked = props.checkedKeys.indexOf(props.treeNode.key) >= 0
+
+    const isHalfCheck =
+        !isChecked &&
+        childrenKeys.some((key) => props.checkedKeys.indexOf(key) >= 0)
+
+    useEffect(() => {
+        if (isChildrenAllChecked && !isChecked) {
+            props.setCheckedKeysFn([...props.checkedKeys, props.treeNode.key])
+        }
+        if (isChildrenAllUnchecked && isChecked) {
+            props.setCheckedKeysFn(
+                props.checkedKeys.filter((key) => key !== props.treeNode.key)
+            )
+        }
+    }, [isChildrenAllChecked, isChildrenAllUnchecked, isChecked, isHalfCheck])
+
+    useEffect(() => {
+        if (ref.current) {
+            ref.current.checked = isChecked
+            ref.current.indeterminate = isHalfCheck
+        }
+    }, [isChecked, isHalfCheck])
+
+    const handleCheck: ChangeEventHandler<HTMLInputElement> = (event) => {
+        let newCheckedKeys = [...props.checkedKeys]
+        if (event.target.checked) {
+            newCheckedKeys.push(props.treeNode.key, ...childrenKeys)
+        } else {
+            newCheckedKeys = newCheckedKeys.filter(
+                (key) =>
+                    key !== props.treeNode.key && childrenKeys.indexOf(key) < 0
+            )
+        }
+        props.setCheckedKeysFn(newCheckedKeys)
+        props.onCheck && props.onCheck(newCheckedKeys, props.treeNode, event)
+    }
+
+    const key = `level/${props.level}-value/${props.treeNode.key}`
+
+    return (
+        <>
+            <div
+                key={key}
+                data-key={key}
+                style={{ marginLeft: props.level * 10 }}
+                className={classname(PREFIX + '-item')}
+            >
+                <span className={PREFIX + '-item-checkbox-wrapper'}>
+                    <input
+                        type="checkbox"
+                        checked={isChecked}
+                        onChange={handleCheck}
+                        data-checked={isChecked}
+                    ></input>
+                    <span
+                        className={PREFIX + '-item-checkbox-indeterminate'}
+                        data-visible={isHalfCheck}
+                    ></span>
+                </span>
+                <span className={PREFIX + '-label'}>
+                    {props.treeNode.title}
+                </span>
+                {props.treeNode.children &&
+                    props.treeNode.children.map((child) => (
+                        <TreeNode
+                            key={child.key}
+                            treeNode={child}
+                            checkedKeys={props.checkedKeys}
+                            setCheckedKeysFn={props.setCheckedKeysFn}
+                            onCheck={props.onCheck}
+                            level={props.level + 1}
+                        ></TreeNode>
+                    ))}
+            </div>
+        </>
+    )
+}
+
 const Tree: React.FC<TreeProps> = (props) => {
     const [checkedKeys, setCheckedKeys] = useState<string[]>([])
 
@@ -46,100 +161,23 @@ const Tree: React.FC<TreeProps> = (props) => {
         }
     }, [props.checkedKeys])
 
-    const renderChildren = (
-        treeData: TreeData,
-        level = 1,
-        parentNode?: TreeItem
-    ) => {
-        const jsxList = treeData.map((item) => {
-            const isChecked = checkedKeys
-                ? checkedKeys.indexOf(item.key) >= 0
-                : false
-            const key = `level/${level}-value/${item.key}`
+    const setCheckedKeysFn = (newCheckedKeys: string[]) =>
+        setCheckedKeys(newCheckedKeys)
 
-            const childrenKeys: string[] = getChildrenKeys(item)
-
-            const checkParent = (checkedKeys: string[]) => {
-                if (parentNode) {
-                    const childrenKeys = getChildrenKeys(parentNode)
-                    const checkedChildrenKeys = checkedKeys.filter(
-                        (key) => childrenKeys.indexOf(key) >= 0
-                    )
-                    const shouldCheckParent =
-                        checkedChildrenKeys.length > 0 &&
-                        checkedChildrenKeys.length === childrenKeys.length
-
-                    if (shouldCheckParent) {
-                        checkedKeys.push(parentNode.key)
-                    } else {
-                        if (checkedKeys.indexOf(parentNode.key) >= 0) {
-                            checkedKeys.splice(
-                                checkedKeys.indexOf(parentNode.key),
-                                1
-                            )
-                        }
-                    }
-                }
-            }
-
-            const handleCheck = (e: ChangeEvent<HTMLInputElement>) => {
-                let newCheckedKeys = checkedKeys
-
-                if (e.target.checked) {
-                    newCheckedKeys = [...checkedKeys, item.key, ...childrenKeys]
-                    // TODO 这里只检查了一层，但是应该不断向上检测
-                    checkParent(newCheckedKeys)
-                } else {
-                    newCheckedKeys = checkedKeys.filter(
-                        (key) =>
-                            key !== item.key && childrenKeys.indexOf(key) < 0
-                    )
-                    checkParent(newCheckedKeys)
-                }
-                setCheckedKeys(newCheckedKeys)
-                props.onCheck && props.onCheck(newCheckedKeys, item, e)
-            }
-
-            const isHalfCheck =
-                !!childrenKeys.find(
-                    (childKey) => checkedKeys.indexOf(childKey) >= 0
-                ) && checkedKeys.indexOf(item.key) < 0
-
-            return (
-                <div
-                    key={key}
-                    data-key={key}
-                    style={{ marginLeft: (level - 1) * 10 }}
-                    className={classname(PREFIX + '-item')}
-                >
-                    <span className={PREFIX + '-item-checkbox-wrapper'}>
-                        <input
-                            type="checkbox"
-                            checked={isChecked}
-                            onChange={handleCheck}
-                            ref={(ref) => {
-                                if (ref) {
-                                    ref.indeterminate = isHalfCheck
-                                }
-                            }}
-                        ></input>
-                        <span
-                            className={PREFIX + '-item-checkbox-indeterminate'}
-                            data-visible={isHalfCheck}
-                        ></span>
-                    </span>
-
-                    <span className={PREFIX + '-label'}>{item.title}</span>
-                    {item.children &&
-                        renderChildren(item.children, level + 1, item)}
-                </div>
-            )
-        })
-
-        return <>{jsxList}</>
-    }
-
-    return <div className={PREFIX}>{renderChildren(props.treeData)}</div>
+    return (
+        <div className={PREFIX}>
+            {props.treeData.map((item) => (
+                <TreeNode
+                    key={item.key}
+                    treeNode={item}
+                    checkedKeys={checkedKeys}
+                    setCheckedKeysFn={setCheckedKeysFn}
+                    onCheck={props.onCheck}
+                    level={1}
+                ></TreeNode>
+            ))}
+        </div>
+    )
 }
 
 export default Tree
