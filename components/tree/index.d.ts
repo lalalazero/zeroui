50755811b6832996e837e2b49a87e15785b55601
lalalazero <zzzero520@hotmail.com>
2020-12-21 10:45:46 +0000

    Deploying to gh-pages from  @ 19b5f214b58fd12c3f9bd98283de5d9fcb7247d1 🚀

---
+++ components/tree/index.d.ts
@@ -0,0 +1,15 @@
+import React, { ChangeEvent } from 'react';
+import './style.scss';
+export declare type TreeNode = {
+    title: string;
+    key: string;
+    children?: TreeNode[];
+};
+export declare type TreeData = TreeNode[];
+export declare type TreeProps = {
+    treeData: TreeNode[];
+    checkedKeys?: string[];
+    onCheck?: (newCheckedKeys: string[], checkItem?: TreeNode, e?: ChangeEvent<HTMLInputElement>) => void;
+};
+declare const Tree: React.FC<TreeProps>;
+export default Tree;
