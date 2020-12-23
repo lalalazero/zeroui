e727c4bed66311b90741474d86f0ae97d37fb24d
lalalazero <zzzero520@hotmail.com>
2020-12-23 07:32:17 +0000

    Deploying to gh-pages from  @ 14def1ea6d5a49cab577bdd85dcc53b8dd6eb6d7 🚀

--- components/tree/index.d.ts
+++ components/tree/index.d.ts
@@ -1,15 +1,21 @@
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, ReactNode } from 'react';
 import './style.scss';
 export declare type TreeNode = {
-    title: string;
+    title: string | ReactNode;
     key: string;
     children?: TreeNode[];
+    expandable?: boolean;
 };
 export declare type TreeData = TreeNode[];
 export declare type TreeProps = {
     treeData: TreeNode[];
     checkedKeys?: string[];
+    defaultCheckedKeys?: string[];
+    defaultExpandKeys?: string[];
+    expandKeys?: string[];
+    defaultExpandAll?: boolean;
     onCheck?: (newCheckedKeys: string[], checkNode: TreeNode, event: ChangeEvent<HTMLInputElement>) => void;
+    onExpand?: (expandKeys: string[], expandNode: TreeNode) => void;
 };
 declare const Tree: React.FC<TreeProps>;
 export default Tree;
