498f6d3da7171466f0b6f84ad466c67f6dd4595d
lalalazero <zzzero520@hotmail.com>
2020-12-24 05:39:02 +0000

    Deploying to gh-pages from  @ 6149cc4a66e287a1bfaca4cc606df0e746c9c111 🚀

--- components/icon/Icon.d.ts
+++ components/icon/Icon.d.ts
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Icon.scss';
-export declare const ICONS: readonly ["like", "edit", "copy", "info", "error", "loading", "alipay", "wechat", "code-open", "code-close", "left", "right", "down", "setting", "emptysearch", "filled-up", "filled-down", "filled-left", "filled-right", "download", "next-double", "prev-double", "search", "zoom-in", "zoom-out", "close", "rotate-left", "rotate-right", "check", "check-filled"];
+export declare const ICONS: readonly ["like", "edit", "copy", "info", "error", "loading", "alipay", "wechat", "code-open", "code-close", "left", "right", "down", "setting", "emptysearch", "filled-up", "filled-down", "filled-left", "filled-right", "download", "next-double", "prev-double", "search", "zoom-in", "zoom-out", "close", "rotate-left", "rotate-right", "check", "check-filled", "happy", "joy"];
 export declare type ICON = typeof ICONS[number];
 export interface IconProps extends React.SVGAttributes<SVGElement> {
     name: ICON;
--- components/tree/index.d.ts
+++ components/tree/index.d.ts
@@ -6,6 +6,7 @@ export declare type TreeNode = {
     children?: TreeNode[];
     expandable?: boolean;
     checkable?: boolean;
+    icon?: ReactNode;
 };
 export declare type TreeData = TreeNode[];
 export declare type TreeProps = {
@@ -16,11 +17,14 @@ export declare type TreeProps = {
     defaultSelectedKeys?: string[];
     expandKeys?: string[];
     selectedKeys?: string[];
+    multiple?: boolean;
     checkable: boolean;
     defaultExpandAll?: boolean;
     onCheck?: (newCheckedKeys: string[], checkNode: TreeNode, event: ChangeEvent<HTMLInputElement>) => void;
     onExpand?: (expandKeys: string[], expandNode: TreeNode) => void;
     onSelect?: (selectedKeys: string[], selectNode: TreeNode) => void;
+    expandIcon?: ReactNode;
+    collapseIcon?: ReactNode;
 };
 declare const Tree: React.FC<TreeProps>;
 export default Tree;
