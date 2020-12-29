022411df07fd47045609167366bcc563abe8f7f7
lalalazero <zzzero520@hotmail.com>
2020-12-29 08:52:02 +0000

    Deploying to gh-pages from  @ 7950f8544f65ce03112a51f16c47632e3ae30a6d 🚀

--- components/input-number/index.d.ts
+++ components/input-number/index.d.ts
@@ -1,5 +1,7 @@
 import React from 'react';
 import './style.scss';
+export declare const NumberInputTypeEnums: readonly ["default", "type2"];
+export declare type NumberInputType = typeof NumberInputTypeEnums[number];
 export interface NumberInputProps {
     min?: number;
     max?: number;
@@ -8,6 +10,8 @@ export interface NumberInputProps {
     value?: number;
     name?: string;
     onChange?: (name: string, value: number) => void;
+    disabled?: boolean;
+    type?: NumberInputType;
 }
 declare const NumberInput: React.FC<NumberInputProps>;
 export default NumberInput;
