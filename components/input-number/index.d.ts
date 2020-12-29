f0db3b08893ac7730358884e76689d8c44a485ac
lalalazero <zzzero520@hotmail.com>
2020-12-29 07:34:25 +0000

    Deploying to gh-pages from  @ c2135670432540e2720c4b3d1ddaa2b4d00591e2 🚀

--- components/input-number/index.d.ts
+++ components/input-number/index.d.ts
@@ -1,3 +1,13 @@
 import React from 'react';
-declare const NumberInput: React.FC<any>;
+import './style.scss';
+export interface NumberInputProps {
+    min?: number;
+    max?: number;
+    step?: number;
+    defaultValue?: number;
+    value?: number;
+    name?: string;
+    onChange?: (name: string, value: number) => void;
+}
+declare const NumberInput: React.FC<NumberInputProps>;
 export default NumberInput;
