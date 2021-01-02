3df5cb1d509a170c8d33aee46f6dc18a0cc9c435
lalalazero <zzzero520@hotmail.com>
2021-01-02 13:40:46 +0000

    Deploying to gh-pages from  @ 88d30f63222f2ab13ca176d47b78eb3083734660 🚀

--- components/rate/index.d.ts
+++ components/rate/index.d.ts
@@ -1,4 +1,9 @@
 import React from 'react';
 import './style.scss';
-declare const Rate: React.FC<any>;
+export interface RateProps {
+    defaultValue?: number;
+    value?: number;
+    disabled?: boolean;
+}
+declare const Rate: React.FC<RateProps>;
 export default Rate;
