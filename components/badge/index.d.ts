e0644d7ad6d003e1d533762ab67ac5b804a956e0
lalalazero <zzzero520@hotmail.com>
2021-01-04 12:26:23 +0000

    Deploying to gh-pages from  @ 4734fca7586a9a37027bb0f0cee6cea65b2d2bf3 🚀

--- components/badge/index.d.ts
+++ components/badge/index.d.ts
@@ -1,10 +1,12 @@
 import React, { CSSProperties } from 'react';
 import './style.scss';
-export interface BadgeProps extends CSSProperties {
+export interface BadgeProps extends Omit<CSSProperties, 'offset'> {
     count?: number;
     className?: string;
     showZero?: boolean;
     overCount?: number;
+    dot?: boolean;
+    offset?: [number | string, number | string];
 }
 declare const Badge: React.FC<BadgeProps>;
 export default Badge;
