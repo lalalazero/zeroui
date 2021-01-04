7baec9b37cf7dfa2c90719d82d9c3665a5aa7e3b
lalalazero <zzzero520@hotmail.com>
2021-01-04 07:39:33 +0000

    Deploying to gh-pages from  @ ab704d65562a957168426258c31cb16026556da9 🚀

--- components/rate/index.d.ts
+++ components/rate/index.d.ts
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import './style.scss';
 export interface RateProps {
     defaultValue?: number;
     value?: number;
     disabled?: boolean;
     allowHalf?: boolean;
+    allowClear?: boolean;
+    tooltips?: string[];
+    charactor?: string | ((index: number) => ReactNode);
+    onChange?: (name: string, value: number | null) => void;
+    name?: string;
 }
 declare const Rate: React.FC<RateProps>;
 export default Rate;
