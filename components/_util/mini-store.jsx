ecf747732f354808349c2ed8b65daf85ed06b826
lalalazero <zzzero520@hotmail.com>
2021-01-14 17:40:19 +0800

    update

--- components/_util/mini-store.jsx
+++ components/_util/mini-store.jsx
@@ -16,32 +16,35 @@ export const connect = (mapStateToProps = defaultMapStateToProps) => {
     return function wrapHOC(WrapComponent) {
         const InnerFC = (props) => {
             const store = useContext(Context)
-            console.log('connect, store=', store.getState())
+            // console.log('connect, store=', store.getState())
 
             const mappedState = mapStateToProps(store.getState())
 
-            console.log('mappedState,', mappedState)
+            // console.log('mappedState,', mappedState)
 
-            const [finalState, setFinalState] = useState(mappedState)
+            // const [finalState, setFinalState] = useState(mappedState)
+
+            const [_, setX] = useState({})
 
             useEffect(() => {
                 const unsub = store.subscribe(() => {
-                    const newState = store.getState()
-                    console.log('newState = ', newState)
+                    // const newState = store.getState()
+                    // console.log('newState = ', newState)
 
-                    const mappedStateNew = mapStateToProps(newState)
-                    setFinalState(mappedStateNew)
+                    // const mappedStateNew = mapStateToProps(newState)
+                    // setFinalState(mappedStateNew)
+                    setX({})
                 })
 
                 return () => {
                     unsub()
-                    console.log('清理 store 的订阅')
+                    // console.log('清理 store 的订阅')
                 }
             }, [])
 
             return (
                 <WrapComponent
-                    {...finalState}
+                    {...mappedState}
                     {...props}
                     store={store}
                 ></WrapComponent>
@@ -70,10 +73,10 @@ export function createStore(initialStore = {}) {
         return store
     }
     function setState(partialStore) {
-        console.log('setState..,partialState', partialStore)
+        // console.log('setState..,partialState', partialStore)
 
         store = { ...store, ...partialStore }
-        console.log('after..store', store)
+        // console.log('after..store', store)
         listeners.forEach((fn) => fn())
     }
     return {
